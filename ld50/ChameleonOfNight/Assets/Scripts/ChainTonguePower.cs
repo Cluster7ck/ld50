257f8debe56f990d45918d86875cc74dc4093308
Robin Schmitz <schmitz.robin@gmx.de>
2022-04-02 21:04:34 +0200

    wip

---
+++ ld50/ChameleonOfNight/Assets/Scripts/ChainTonguePower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChainTonguePower : PowerUp
+{
+	private string name = "ChainTongue";
+	private float range = 0.2f;
+
+	public ChainTonguePower(int level = 1)
+	{
+		range *= level * 0.2f;
+	}
+
+	public override void Power() {
+		Debug.Log(name);
+		List<Enemy> enemies = GameObject.g
+    }
+}
---
+++ ld50/ChameleonOfNight/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class PowerUp
+{
+	string name = "PowerUp";
+	public PowerUp()
+	{
+		
+	}
+	public virtual void Power () {
+		Debug.Log(name);
+    }
+}
