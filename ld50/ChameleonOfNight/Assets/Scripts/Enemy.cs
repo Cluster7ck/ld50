7e1a4079a2e928ff996edfcd9b8837069cfa219c
Robin Schmitz <schmitz.robin@gmx.de>
2022-04-02 16:59:42 +0200

    bzzzzt animation

--- ld50/ChameleonOfNight/Assets/Scripts/Enemy.cs
+++ ld50/ChameleonOfNight/Assets/Scripts/Enemy.cs
@@ -6,22 +6,25 @@ using DG.Tweening;
 public class Enemy : MonoBehaviour
 {
     private GameObject _flyTarget;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    [SerializeField] private float _flightTime = 10;
+    [SerializeField] private int _flightDirectionChanges = 10;
 
     public void SetTarget (GameObject target) {
         _flyTarget = target;
-        Vector3 targetDirection = transform.position - _flyTarget.transform.position;
 
-        transform.DOMove(_flyTarget.transform.position, 3f);
+        transform.DOPath(flightPath(), _flightTime);
+    }
+
+    private Vector3[] flightPath() {
+        Vector3[] _path = new Vector3[_flightDirectionChanges];
+        float distanceToTarget = (transform.position - _flyTarget.transform.position).magnitude;
+        _path[0] = transform.position;
+        for(int i = 1; i < _flightDirectionChanges; i++) {
+            Vector3 targetDirection = _path[i-1] - _flyTarget.transform.position;
+            Vector3 newWaypoint = targetDirection * (distanceToTarget/(_flightDirectionChanges*i)); // waypoint calculated, so that distance covered is relative to distance to target left
+            newWaypoint = new Vector3(newWaypoint.x, 0.1f, newWaypoint.z);
+            _path[i] = newWaypoint;
+        }
+        return _path;
     }
 }
